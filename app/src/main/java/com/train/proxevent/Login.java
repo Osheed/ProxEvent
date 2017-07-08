d71a33a4f1ffd8bb0f81e3338a874883aead04c1
Helder <Osheed_@hotmail.com>
2017-07-08 12:09:20 +0200

    test

--- app/src/main/java/com/train/proxevent/Login.java
+++ app/src/main/java/com/train/proxevent/Login.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Login extends AppCompatActivity {
-//gggg
+//gggg5
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

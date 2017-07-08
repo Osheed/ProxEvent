a12ec9ead178abe7e769a918c362553a16c18cf6
Helder <Osheed_@hotmail.com>
2017-07-08 12:08:40 +0200

    test

---
+++ app/src/main/java/com/train/proxevent/Login.java
@@ -0,0 +1,13 @@
+package com.train.proxevent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+//gggg
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

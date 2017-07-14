181ac441b372b14097185b6af4a694270a143de9
Helder <Osheed_@hotmail.com>
2017-07-14 15:34:52 +0200

    Add an Activity page Controllers for fields Field determinate/indeterminate Date picker

--- app/src/main/java/com/train/proxevent/new_activity.java
+++ app/src/main/java/com/train/proxevent/new_activity.java
@@ -1,14 +1,57 @@
 package com.train.proxevent;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
 
 public class new_activity extends AppCompatActivity {
 
-    @Override
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_activity);
         setTitle(R.string.newActivity);
+
+        mDisplayDate = (TextView)findViewById(R.id.tvDate_New_Activity);
+
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                int month = cal.get(Calendar.MONTH);
+                int year = cal.get(Calendar.YEAR);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        new_activity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        day,month,year);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable((Color.TRANSPARENT)));
+                dialog.show();
+            }
+        });
+            mDateSetListener = new DatePickerDialog.OnDateSetListener(){
+
+                @Override
+                public void onDateSet(DatePicker datePicker, int day, int month, int year) {
+                    month = month +1;
+
+                    String date = day + "/" + month + "/" +year;
+                    mDisplayDate.setText(date);
+                }
+            };
     }
 }

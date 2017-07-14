1e7110ac7dd9a4de5707452fa853787ccac7793a
xavne <xavnen88@hotmail.com>
2017-07-14 09:58:39 +0200

    start menu

--- app/src/main/java/com/train/proxevent/home.java
+++ app/src/main/java/com/train/proxevent/home.java
@@ -3,6 +3,8 @@ package com.train.proxevent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -18,7 +20,6 @@ public class home extends AppCompatActivity {
     public FirebaseAuth Auth = FirebaseAuth.getInstance();
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,23 +27,79 @@ public class home extends AppCompatActivity {
 
         Intent intent = getIntent();
 
-       /**
-        *
-        *    SELECT * FROM user WHERE email = 'user.getEmail()';
-        *
-        *
-        *    ==
-        *
-        *    new Firebase("https://examples-sql-queries.firebaseio.com/user")
-    .startAt('user.getEmail()')
-                .endAt('user.getEmail()')
-                .once('value', show);
+        /**
+         *
+         *    SELECT * FROM user WHERE email = 'user.getEmail()';
+         *
+         *
+         *    ==
+         *
+         *    new Firebase("https://examples-sql-queries.firebaseio.com/user")
+         .startAt('user.getEmail()')
+         .endAt('user.getEmail()')
+         .once('value', show);
 
-        function show(snap) {
-                $('pre').text(JSON.stringify(snap.val(), null, 2));
+         function show(snap) {
+         $('pre').text(JSON.stringify(snap.val(), null, 2));
+         }
+
+         **/
+    }
+
+
+    /* menu */
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_home_map:
+                Intent goHomeMap = new Intent(this, home.class);
+                startActivity(goHomeMap);
+                return true;
+
+            case R.id.action_topics:
+                Intent goTopics = new Intent(this, home.class);
+                startActivity(goTopics);
+                return true;
+
+            case R.id.action_add_activity:
+                Intent goAdd = new Intent(this, home.class);
+                startActivity(goAdd);
+                return true;
+
+            case R.id.action_messages:
+                Intent goMessage = new Intent(this, home.class);
+                startActivity(goMessage);
+                return true;
+
+            case R.id.action_my_activities:
+                Intent goActicities = new Intent(this, home.class);
+                startActivity(goActicities);
+                return true;
+
+            case R.id.action_favorite:
+                Intent goFavorite = new Intent(this, home.class);
+                startActivity(goFavorite);
+                return true;
+
+            case R.id.action_profile:
+                Intent goProfile = new Intent(this, home.class);
+                startActivity(goProfile);
+                return true;
+
+            case R.id.action_settings:
+                Intent goSettings = new Intent(this, home.class);
+                startActivity(goSettings);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        **/
-
     }
 }
+

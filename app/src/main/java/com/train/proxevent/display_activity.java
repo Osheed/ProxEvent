d3cf10d528ea918928c56d00d16e7e134ac907ec
xavne <xavnen88@hotmail.com>
2017-08-27 12:26:58 +0200

    chat

--- app/src/main/java/com/train/proxevent/display_activity.java
+++ app/src/main/java/com/train/proxevent/display_activity.java
@@ -1,13 +1,86 @@
 package com.train.proxevent;
 
+import android.content.Context;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class display_activity extends AppCompatActivity {
+
+
+    Button saveMessage;
+    EditText etAddMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_activity);
+
+
+        etAddMessage = (EditText) findViewById(R.id.etAddMessage);
+
+
+        //save a comment when user click on save
+        saveMessage = (Button) findViewById(R.id.btSaveMsg);
+        saveMessage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String comment = etAddMessage.getText().toString();
+                if (TextUtils.isEmpty(comment)) {
+                    Toast.makeText(getBaseContext(), R.string.enterChat, Toast.LENGTH_SHORT).show();
+
+                }else{
+                    SimpleDateFormat time = new SimpleDateFormat("dd.MM.yyyy - HH:mm");
+                    String currentTime = time.format(new Date());
+                    String content = etAddMessage.getText().toString();
+                    //String author = usernameSharedPref.toString();
+
+                    // add comment in database
+
+                    // Write a message to the database
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("Chat");
+                    myRef.setValue(content);
+
+                    //dbHelper.addComment(content, currentTime, author, myValueKeyIdQuestion);
+                    Toast.makeText(getBaseContext(), R.string.commentAdded, Toast.LENGTH_SHORT).show();
+
+
+                    // Clear and set hint in EditText addComment
+                    etAddMessage.getText().clear();
+                    etAddMessage.setHint(R.string.addComment);
+
+
+                    // Close the keyboard on save comment
+                    InputMethodManager inputManager = (InputMethodManager)
+                            getSystemService(Context.INPUT_METHOD_SERVICE);
+
+                    inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                            InputMethodManager.HIDE_NOT_ALWAYS);
+
+
+
+                }
+            }
+        });
+
+
     }
+
+
+
+
 }
+
+
--- app/src/main/java/com/train/proxevent/home.java
+++ app/src/main/java/com/train/proxevent/home.java
@@ -1,13 +1,25 @@
 package com.train.proxevent;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.train.proxevent.Objects.User;
 
 /**
  * Created by David on 14.07.2017.
@@ -25,7 +37,6 @@ public class home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         setTitle(R.string.Home);
-
 
 
         Intent intent = getIntent();
@@ -47,6 +58,16 @@ public class home extends AppCompatActivity {
          }
 
          **/
+    }
+
+
+
+    // When the user click on Login button
+    public void seeDisplayActivityTest(View view) {
+        Intent goTopics = new Intent(this, display_activity.class);
+        startActivity(goTopics);
+
+
     }
 
 

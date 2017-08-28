bd81f2480dee404e4c32900f2cd3d30e8d0afdb7
Helder <Osheed_@hotmail.com>
2017-08-28 22:22:03 +0200

    profile / Status activity

---
+++ app/src/main/java/com/train/proxevent/StatusActivity.java
@@ -0,0 +1,85 @@
+package com.train.proxevent;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class StatusActivity extends AppCompatActivity {
+
+
+    private TextInputLayout mStatus;
+    private Button mSavebtn;
+    private DatabaseReference mStatusDb;
+    private FirebaseUser mCurrentUser;
+
+    //Progress
+    private ProgressDialog mProgress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+
+        mStatus = (TextInputLayout) findViewById(R.id.til_status_change);
+        mSavebtn = (Button) findViewById(R.id.btn_status_change);
+
+        //Firebase
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String current_id = mCurrentUser.getUid();
+        mStatusDb = FirebaseDatabase.getInstance().getReference().child("Users").child(current_id);
+
+        //step to retrieve the old status
+        String status_value = getIntent().getStringExtra("status_value");
+        mStatus.getEditText().setHint(status_value);
+
+        mSavebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Progress
+                mProgress = new ProgressDialog(StatusActivity.this);
+                mProgress.setTitle("Saving Changes");
+                mProgress.setMessage("Please wait while we save the changes");
+                mProgress.show();
+
+                //Recover data and sent to the EditText to wiev old status
+                String status = mStatus.getEditText().getText().toString();
+
+                mStatusDb.child("status").setValue(status).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if(task.isSuccessful()){
+
+                            mProgress.dismiss();
+                            Intent profileIntent = new Intent(StatusActivity.this, profile.class);
+                            startActivity(profileIntent);
+
+                        }else{
+
+                            Toast.makeText(getApplicationContext(),"There was some errors in saving Changes",Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+
+
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/train/proxevent/profile.java
+++ app/src/main/java/com/train/proxevent/profile.java
@@ -1,14 +1,207 @@
 package com.train.proxevent;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class profile extends AppCompatActivity {
+
+
+    private DatabaseReference mUserDatabase;
+    private FirebaseUser mCurrentUser;
+
+    //Android Layout
+    private CircleImageView mDisplayImage;
+    private TextView mName;
+    private TextView mStatus;
+    private Button mStatusBtn;
+    private Button mImageBtn;
+
+    private static final int GALLERY_PICK = 1;
+
+    //Storage Firebase
+    private StorageReference mImageStorage;
+
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         setTitle(R.string.profile);
+
+
+        //Initializes
+        mDisplayImage = (CircleImageView) findViewById(R.id.civ_profile_image);
+        mName = (TextView) findViewById(R.id.tv_profile_displayName);
+        mStatus = (TextView) findViewById(R.id.tv_profile_status);
+        mStatusBtn = (Button) findViewById(R.id.btn_settings_chStatus);
+        mImageBtn = (Button) findViewById(R.id.btn_profile_chImage);
+        mImageStorage = FirebaseStorage.getInstance().getReference();
+
+        //Recover the data from db
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String current_uid = mCurrentUser.getUid();
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(current_uid);
+        mUserDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String name = dataSnapshot.child("name").getValue().toString();
+                String image = dataSnapshot.child("image").getValue().toString();
+                String status = dataSnapshot.child("status").getValue().toString();
+                String thumb_image = dataSnapshot.child("thumb_image").getValue().toString();
+
+                mName.setText(name);
+                mStatus.setText(status);
+
+                //load image
+                Picasso.with(profile.this).load(image).into(mDisplayImage);
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+
+            }
+        });
+
+        mStatusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String status_value = mStatus.getText().toString();
+                Intent status_intent = new Intent(profile.this, StatusActivity.class);
+                status_intent.putExtra("status_value", status_value);
+                startActivity(status_intent);
+
+            }
+        });
+
+        mImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                //Recover image in the phone
+                Intent galleryIntent = new Intent();
+                galleryIntent.setType("image/*");
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(galleryIntent, "SELECT IMAGE"), GALLERY_PICK);
+
+
+                /*
+                // start picker to get image for cropping and then use the image in cropping activity
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(SettingsActivity.this);
+
+                 */
+
+            }
+        });
+
+
+    }
+
+    //Recover the Crop Image
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_PICK && resultCode == RESULT_OK) {
+
+            Uri imageUri = data.getData();
+
+            // start cropping activity for pre-acquired image saved on the device
+            CropImage.activity(imageUri)
+                    .setAspectRatio(1, 1)
+                    .start(this);
+
+
+        }
+        // Checks and retrieves the uri of the image
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+            if (resultCode == RESULT_OK) {
+
+                //Progress Bar for the User
+                mProgressDialog = new ProgressDialog(profile.this);
+                mProgressDialog.setTitle("Uploading Image...");
+                mProgressDialog.setMessage("Please wait while we upload and process the image");
+                mProgressDialog.setCanceledOnTouchOutside(false);
+                mProgressDialog.show();
+
+
+                Uri resultUri = result.getUri();
+
+                String current_user_id = mCurrentUser.getUid();
+                StorageReference filepath = mImageStorage.child("profile_images").child(current_user_id +".jpg");
+
+                filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+
+                    @SuppressWarnings("VisibleForTests")
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                        if(task.isSuccessful()){
+
+                            //Store in the user DB
+                            String download_url = task.getResult().getDownloadUrl().toString();
+
+                            mUserDatabase.child("image").setValue(download_url).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                    if(task.isSuccessful()){
+
+                                        mProgressDialog.dismiss();
+
+                                    }
+                                }
+                            });
+
+                        }else{
+
+                            Toast.makeText(profile.this, "Error in uploading",Toast.LENGTH_LONG).show();
+                            mProgressDialog.dismiss();
+                        }
+
+                    }
+                });
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+
+            }
+        }
+
     }
 }

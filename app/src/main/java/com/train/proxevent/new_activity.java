ca16f7d2f0e3d6be5169e90646bee4357c2a1c64
Helder <Osheed_@hotmail.com>
2017-08-30 21:25:15 +0200

    new activity 3e etape

--- app/src/main/java/com/train/proxevent/new_activity.java
+++ app/src/main/java/com/train/proxevent/new_activity.java
@@ -23,8 +23,10 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -32,7 +34,10 @@ import com.theartofdev.edmodo.cropper.CropImage;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -45,7 +50,6 @@ public class new_activity extends AppCompatActivity {
 
     private TextView mDisplayDate;
     private Spinner mTopics;
-    private DatePickerDialog.OnDateSetListener mDateSetListener;
     private EditText mEnterTitle;
     private EditText mEnterContent;
     private CheckBox mPermanent;
@@ -54,16 +58,22 @@ public class new_activity extends AppCompatActivity {
     private Button mSave;
     private CircleImageView mImage;
     private static final int GALLERY_PICK = 1;
-    private ProgressDialog mProgressDialog;
+    private String topics;
+    private String dateEnd;
+    //Progress
+    private ProgressDialog mProgress;
     //Storage Firebase
     private StorageReference mImageStorage;
     private FirebaseUser mCurrentUser;
     private DatabaseReference mActivityDatabase;
+    private int mYear, mMonth, mDay;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_activity);
         setTitle(R.string.newActivity);
+        final Calendar c = Calendar.getInstance();
 
         //Initializes
         mDisplayDate = (TextView) findViewById(R.id.tv_newActivity_SelectDate);
@@ -77,6 +87,10 @@ public class new_activity extends AppCompatActivity {
         mImage = (CircleImageView) findViewById(R.id.civ_newActivity_ChooseImg);
         mImageStorage = FirebaseStorage.getInstance().getReference();
 
+        //Database
+        mActivityDatabase = FirebaseDatabase.getInstance().getReference().child("Activities");
+
+
         //choose the Topic
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 R.layout.support_simple_spinner_dropdown_item,
@@ -86,6 +100,7 @@ public class new_activity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
                 Toast.makeText(new_activity.this, adapterView.getItemAtPosition(position).toString(),Toast.LENGTH_LONG).show();
+               topics = adapterView.getItemAtPosition(position).toString();
             }
 
             @Override
@@ -94,45 +109,99 @@ public class new_activity extends AppCompatActivity {
             }
         });
 
-
-
-
-
         //Choose the Date
         mDisplayDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Calendar cal = Calendar.getInstance();
-                int day = cal.get(Calendar.DAY_OF_MONTH);
-                int month = cal.get(Calendar.MONTH);
-                int year = cal.get(Calendar.YEAR);
-
-                DatePickerDialog dialog = new DatePickerDialog(
-                        new_activity.this,
-                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
-                        mDateSetListener,
-                        day, month, year);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable((Color.TRANSPARENT)));
-                dialog.show();
+                DatePickerDialog dpd = new DatePickerDialog(new_activity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int day) {
+                                c.set(year, month, day);
+                                String date = new SimpleDateFormat("dd-MM-yyyy").format(c.getTime());
+                                mDisplayDate.setText(date);
+                                mYear = c.get(Calendar.YEAR);
+                                mMonth = c.get(Calendar.MONTH);
+                                mDay = c.get(Calendar.DAY_OF_MONTH);
+                            }
+                        }, mYear, mMonth, mDay);
+                dpd.getDatePicker().setMinDate(System.currentTimeMillis());
+                Calendar d = Calendar.getInstance();
+                d.add(Calendar.MONTH,1);
+                dpd.getDatePicker().setMaxDate(d.getTimeInMillis());
+                dpd.show();
             }
+
         });
+/*
+        //Display the date
         mDateSetListener = new DatePickerDialog.OnDateSetListener() {
 
             @Override
-            public void onDateSet(DatePicker datePicker, int day, int month, int year) {
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
                 month = month + 1;
 
-                String date = day + "/" + month + "/" + year;
-                mDisplayDate.setText(date);
+                dateEnd = day + "-" + month + "-" + year;
+                mDisplayDate.setText(dateEnd);
             }
         };
+*/
 
         mSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                //Progress
+                mProgress = new ProgressDialog(new_activity.this);
+                mProgress.setTitle("Saving Changes");
+                mProgress.setMessage("Please wait while we save the changes");
+                mProgress.show();
+
+                //Recover data
+                String content = mEnterContent.getText().toString().trim();
+                Date todayDate = Calendar.getInstance().getTime();
+                SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+                String dateCrea = formatter.format(todayDate);
+                String dateEnd = mDisplayDate.getText().toString().trim();
+                String latitude = "latitudeyyyy";
+                String longitude = "longitudexxx";
+                String adresse = "adresse-test";
+                mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+                String current_id = mCurrentUser.getUid();
+                String title = mEnterTitle.getText().toString().trim();
 
 
+                // to ordenate data for db
+                HashMap< String, String> activityMap = new HashMap<>();
+                activityMap.put("adresse", adresse);
+                activityMap.put("content",content);
+                activityMap.put("date_crea",dateCrea);
+                activityMap.put("date_end",dateEnd);
+                activityMap.put("latitude",latitude);
+                activityMap.put("longitude",longitude);
+                activityMap.put("owner",current_id);
+                activityMap.put("title",title);
+                activityMap.put("topic",topics);
+
+                mActivityDatabase.push().setValue(activityMap).
+                        addOnCompleteListener(new OnCompleteListener<Void>() {
+                             @Override
+                             public void onComplete(@NonNull Task<Void> task) {
+
+                                 if(task.isSuccessful()){
+
+                                     mProgress.dismiss();
+                                     Intent profileIntent = new Intent(new_activity.this, home.class);
+                                     startActivity(profileIntent);
+
+                                 }else{
+
+                                     Toast.makeText(getApplicationContext(),"There was some errors in Creating this Activity",Toast.LENGTH_LONG).show();
+
+                                 }
+                             }
+                        }
+                );
             }
         });
 

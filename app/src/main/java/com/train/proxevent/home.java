bceaaf0c7b5e9e6a5bd004189f8b3d6b0733a54a
Helder <Osheed_@hotmail.com>
2017-09-02 15:11:58 +0200

    Home avec liste activités triés par end_date (2h)

--- app/src/main/java/com/train/proxevent/home.java
+++ app/src/main/java/com/train/proxevent/home.java
@@ -1,17 +1,23 @@
 package com.train.proxevent;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,9 +30,18 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+import com.train.proxevent.Objects.Activities;
 import com.train.proxevent.Objects.User;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by David on 14.07.2017.
@@ -42,6 +57,8 @@ import com.train.proxevent.Objects.User;
 public class home extends AppCompatActivity implements OnMapReadyCallback {
 
     private FirebaseAuth mAuth;
+    private RecyclerView rv_currActivities;
+    private DatabaseReference mActivityDatabase;
 
     private GoogleMap mymap;
 
@@ -51,6 +68,16 @@ public class home extends AppCompatActivity implements OnMapReadyCallback {
         setContentView(R.layout.activity_home);
         setTitle(R.string.Home);
 
+        //Current activities
+        rv_currActivities = (RecyclerView) findViewById(R.id.rv_home_currActivities);
+        rv_currActivities.setHasFixedSize(true);
+        rv_currActivities.setLayoutManager(new LinearLayoutManager(this));
+
+        mActivityDatabase = FirebaseDatabase.getInstance().getReference("Activities").child("All");
+        Query sortByDate = mActivityDatabase.orderByChild("Act_date_end");
+
+
+        // Map
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
 
@@ -94,16 +121,6 @@ public class home extends AppCompatActivity implements OnMapReadyCallback {
 
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-
-        if(currentUser == null){
-            sendToStart();
-        }
-    }
 
     private void sendToStart() {
         Intent loginIntent = new Intent(home.this, LoginActivity.class);
@@ -174,5 +191,105 @@ public class home extends AppCompatActivity implements OnMapReadyCallback {
         mymap.moveCamera(camUpd1);
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser == null){
+            sendToStart();
+        }
+
+
+        FirebaseRecyclerAdapter<Activities, activity_list.ActivityViewHolder> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter <Activities, activity_list.ActivityViewHolder>(
+                        Activities.class,
+                        R.layout.activity_list_layout,
+                        activity_list.ActivityViewHolder.class,
+                        mActivityDatabase.orderByChild("Act_date_end")
+                ){
+                    @Override
+                    protected void populateViewHolder(activity_list.ActivityViewHolder viewHolder, Activities model, int position){
+
+
+                        viewHolder.setAdresse(model.getAct_adresse());
+                        //viewHolder.setContent(model.getAct_content());
+                        viewHolder.setDate_crea(model.getAct_date_crea());
+                        viewHolder.setDate_end(model.getAct_date_end());
+                        //viewHolder.setLatitude(model.getAct_latitude());
+                        //viewHolder.setLongitude(model.getAct_longitude());
+                        //viewHolder.setOwner(model.getAct_owner());
+                        viewHolder.setTitle(model.getAct_title());
+                        //viewHolder.setTopic(model.getAct_topic());
+                        viewHolder.setActImage(model.getAct_image(),getApplicationContext());
+
+
+                        //retrieve the key of activity clicked
+                        final String activity_id = getRef(position).getKey();
+
+                        viewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent activityIntent = new Intent(home.this, display_activity.class);
+                                activityIntent.putExtra("idActivity", activity_id);
+                                startActivity(activityIntent);
+                            }
+                        });
+
+                    }
+
+
+                };
+
+        rv_currActivities.setAdapter(firebaseRecyclerAdapter);
+
+    }
+    //to retrieve the data
+    public static class ActivityViewHolder extends RecyclerView.ViewHolder{
+
+        View mView;
+
+        public ActivityViewHolder(View itemView){
+            super(itemView);
+
+            mView = itemView;
+        }
+
+
+        public void setAdresse(String adresse) {
+            TextView activityAdress = (TextView) mView.findViewById(R.id.tv_AL_Adresse);
+            activityAdress.setText(adresse);
+        }
+
+        public void setDate_crea(String date_crea){
+            TextView activityDate_crea = (TextView) mView.findViewById(R.id.tv_AL_dateCrea);
+            activityDate_crea.setText(date_crea);
+        }
+
+        public void setDate_end(String date_end){
+            TextView activityDate_end = (TextView) mView.findViewById(R.id.tv_AL_dateEnd);
+            activityDate_end.setText(date_end);
+        }
+
+        public void setTitle(String title){
+            TextView activityTitle = (TextView) mView.findViewById(R.id.tv_AL_Title);
+            activityTitle.setText(title);
+        }
+
+        public void setActImage(String act_image, Context applicationContext) {
+            CircleImageView activityImage = (CircleImageView)mView.findViewById(R.id.civ_AL_image);
+            Picasso.with(applicationContext).load(act_image).placeholder(R.drawable.ic_action_clock).into(activityImage);
+        }
+    }
+
+
+
+
+
+
+
+
 }
 

00a518445fe5adbe9ce8bddc9fb0cb3178f9df66
David Cano Marquez <david.canomarquez@students.hevs.ch>
2017-09-06 20:06:01 +0200

    test map

--- app/src/main/java/com/train/proxevent/home.java
+++ app/src/main/java/com/train/proxevent/home.java
@@ -1,188 +1,202 @@
 package com.train.proxevent;
 
-import android.content.Context;
-import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.squareup.picasso.Picasso;
-import com.train.proxevent.Objects.Activities;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+public class home extends FragmentActivity implements
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        GoogleMap.OnMarkerDragListener,
+        GoogleMap.OnMapLongClickListener,
+        View.OnClickListener {
 
-import de.hdodenhof.circleimageview.CircleImageView;
+    //Our Map
+    private GoogleMap mMap;
 
-/**
- * Created by David on 14.07.2017.
- */
+    //To store longitude and latitude from map
+    private double longitude;
+    private double latitude;
 
-//TODO: Fragment carte dans home et fragment activity list dans le 2e
-//TODO: Delete l'utilisateur
-//TODO: Controler que dans home l'affichage des activités se fait a partir d'aujourd'hui
-//TODO: Detruire une activity si on est le proprietaire
-//TODO: Sortir tous les strings comme dans display activity
+    //Buttons
+    private ImageButton buttonSave;
+    private ImageButton buttonCurrent;
+    private ImageButton buttonView;
 
-public class home extends AppCompatActivity  {
+    //Google ApiClient
+    private GoogleApiClient googleApiClient;
 
-    private FirebaseAuth mAuth;
-
-
-    private Button goAdminBtn;
-    private ViewPager mViewPager;
-    private HomePagerAdapter mHomePagerAdapter;
-    private TabLayout mTabLayout;
-
-    private GoogleMap mymap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        setTitle(R.string.Home);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
-        // initializes
-        mAuth = FirebaseAuth.getInstance();
-        mViewPager = (ViewPager)findViewById(R.id.home_tabPager);
+        //Initializing googleapi client
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
 
-        mHomePagerAdapter = new HomePagerAdapter(getSupportFragmentManager());
-
-        //Fragments
-        mViewPager.setAdapter(mHomePagerAdapter);
-        mTabLayout = (TabLayout)findViewById(R.id.home_tabs);
-        mTabLayout.setupWithViewPager(mViewPager);
-
-
+        //Initializing views and adding onclick listeners
+        buttonSave = (ImageButton) findViewById(R.id.buttonSave);
+        buttonCurrent = (ImageButton) findViewById(R.id.buttonCurrent);
+        buttonView = (ImageButton) findViewById(R.id.buttonView);
+        buttonSave.setOnClickListener(this);
+        buttonCurrent.setOnClickListener(this);
+        buttonView.setOnClickListener(this);
     }
-
-
-    // When the user click on Login button
-    public void seeDisplayActivityTest(View view) {
-        Intent goTopics = new Intent(this, display_activity.class);
-        startActivity(goTopics);
-
-
-    }
-
-    public void goAdmin(View view) {
-        //  Intent goToAdmin = new Intent(this, messageFromAdmin.class);
-        Intent goToAdmin = new Intent(this, admin.class);
-        startActivity(goToAdmin);
-
-
-    }
-    public void goAdminMessages(View view) {
-        Intent goToMessages = new Intent(this, messageFromAdmin.class);
-        startActivity(goToMessages);
-
-
-    }
-
-
-    private void sendToStart() {
-        Intent loginIntent = new Intent(home.this, LoginActivity.class);
-        startActivity(loginIntent);
-        finish();
-    }
-
-
-    /* menu */
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-            case R.id.action_home_map:
-                Intent goHomeMap = new Intent(this, home.class);
-                startActivity(goHomeMap);
-                return true;
-
-            case R.id.action_topics:
-                Intent goTopics = new Intent(this, topic_list.class);
-                startActivity(goTopics);
-                return true;
-
-            case R.id.action_add_activity:
-                Intent goAdd = new Intent(this, new_activity.class);
-                startActivity(goAdd);
-                return true;
-
-            case R.id.action_messages:
-                Intent goMessage = new Intent(this, notification_list.class);
-                startActivity(goMessage);
-                return true;
-
-            case R.id.action_my_activities:
-                Intent goActicities = new Intent(this, my_activities.class);
-                startActivity(goActicities);
-                return true;
-
-            case R.id.action_profile:
-                Intent goProfile = new Intent(this, profile.class);
-                startActivity(goProfile);
-                return true;
-
-            case R.id.action_settings:
-                Intent goSettings = new Intent(this, settings.class);
-                startActivity(goSettings);
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
 
     @Override
     protected void onStart() {
+        googleApiClient.connect();
         super.onStart();
-        // Check if user is signed
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
 
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
 
-        if (currentUser == null) {
-            sendToStart();
+    //Getting current location
+    private void getCurrentLocation() {
+        mMap.clear();
+        //Creating a location object
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        if (location != null) {
+            //Getting longitude and latitude
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
 
-/*
-        // FirebaseUser currentUser = mAuth.getCurrentUser();
-        // Log.i("currentUser", "you are admin + "+currentUser.getUid());
-        String uid = currentUser.getUid();
-        String admin = "mJ4aJAWSSeeQ8mEcAtTySQlPJOU2";
 
-        if (uid.equalsIgnoreCase(admin)) {
-            goAdminBtn = (Button) findViewById(R.id.goAdmin);
-            goAdminBtn.setVisibility(View.VISIBLE);
+
+            //moving the map to location
+            moveMap();
         }
-*/
+    }
+
+    //Function to move the map
+    private void moveMap() {
+        //String to display current latitude and longitude
+        String msg = latitude + ", "+longitude;
+
+        //Creating a LatLng Object to store Coordinates
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        //Adding marker to map
+        mMap.addMarker(new MarkerOptions()
+                .position(latLng) //setting position
+                .draggable(true) //Making the marker draggable
+                .title("Current Location")); //Adding a title
+
+        //Moving the camera
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        //Animating the camera
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(3));
+
+        //Displaying current coordinates in toast
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+     /**   Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();**/
+        LatLng latLng = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(latLng).draggable(true));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.setOnMarkerDragListener(this);
+        mMap.setOnMapLongClickListener(this);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        getCurrentLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
 
     }
 
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
 
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        //Clearing all the markers
+        mMap.clear();
+
+        //Adding a new marker to the current pressed position
+        mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .draggable(true));
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+        //Getting the coordinates
+        latitude = marker.getPosition().latitude;
+        longitude = marker.getPosition().longitude;
+
+        //Moving the map
+        moveMap();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonCurrent){
+            getCurrentLocation();
+            moveMap();
+        }
+    }
 }
-
